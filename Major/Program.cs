fa73ba101cdf888153188762a83b207aeafa7233
max-student <max-student@maxtrain.com>
2020-02-11 08:23:00 -0500

    Initial Commit

---
+++ Major/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MajorProject {
+    class Program {
+        static void Main(string[] args) {
+           
+            var accounting = new  Major (100,1000, "Accounting");
+            var business = new Major(200, 1000, "Busines");
+            var engineering = new Major(300,1200, "Engineering");
+
+            accounting.Print();
+            business.Print();
+            engineering.Print();
+
+
+
+            }
+        }
+    }
